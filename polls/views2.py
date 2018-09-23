e7990300156460a61aab0963a204ab0c44fe0198
REBOOTERS <foreverlove.zyl@163.com>
2018-09-23 14:54:22 +0800

    update view

--- polls/views2.py
+++ polls/views2.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
-
 from .models import Choice, Question
 
 
@@ -11,8 +11,9 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
-
+        return Question.objects.filter(pub_date__lte=timezone.now()
+                                       ).order_by('pub_date')[:5]
+        
 
 class DetailView(generic.DetailView):
     model = Question
